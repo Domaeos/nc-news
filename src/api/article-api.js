c113bf208248623033f80342e04f563dcd9c4248
Domaeos <ryanmurphy.ic@icloud.com>
2023-10-24 12:39:44 +0100

    merge issues

---
+++ src/api/article-api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+const newsAPI = axios.create({
+    baseURL: "https://news-app-4jdh.onrender.com/api/"
+})
+
+
+export const getArticles = async () => {
+    const result = await newsAPI.get("/articles/");
+    return result.data.articles;
+}
+
+export const getSingleArticle = async (articleID) => {
+    const result = await newsAPI.get("/articles/" + articleID);
+    return result.data.article[0];
+}
